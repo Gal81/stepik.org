d4651d39e0cbf506eddc62630031784580bb8800
Valhalleon <v.galuta@gmail.com>
2021-10-31 17:41:24 +0200

    Step2: Spiral

--- base/step2/2.6.5.py
+++ base/step2/2.6.5.py
@@ -1,36 +1,26 @@
+# size = int(input())
 size = 5
 
 matrix = [[0] * size for x in range(size)]
 
 num = 0
-# stop = size ** 2
-stop = 25
-for i in range(size):
-    for j in range(i, size):
-        num += 1
-        if num > stop:
-            break
-        matrix[i][j] = num
-        # print(f'>[{i},{j}]', num)
-        if j == size - i - 1:
-            for x in range(i + 1, size - i):
-                num += 1
-                if num > stop:
-                    break
-                matrix[x][j] = num
-                # print('>>', num)
-                if x == size - i - 1:
-                    for y in range(x - 1, i - 1, -1):
-                        num += 1
-                        if num > stop:
-                            break
-                        matrix[x][y] = num
-                        if y == i:
-                            for z in range(size - i - 2, i, -1):
-                                num += 1
-                                if num > stop:
-                                    break
-                                matrix[z][y] = num
+def fill_cell(x, y):
+    global num
+    num += 1
+    matrix[x][y] = num
+
+for i in range((size // 2 + size % 2)):
+    for j in range(i, size - i):
+        fill_cell(i, j)
+
+    for j in range(i + 1, size - i):
+        fill_cell(j, size - i - 1)
+
+    for j in range(size - i - 2, i - 1, -1):
+        fill_cell(size - i - 1, j)
+
+    for j in range(size - i - 2, i, -1):
+        fill_cell(j, i)
 
 print()
 for row in matrix:
